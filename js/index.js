bf287a9d68ca8c3a7af72db5a3266227ba0a94db
alialaba <alialaba079@gmail.com>
2021-09-27 19:59:24 -0700

    styled the mobile header component

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let headerEl = document.querySelector('.header');
+let btnEl = document.querySelector('.btn-mobile-nav');
+
+btnEl.addEventListener('click', () => {
+    headerEl.classList.toggle('nav-open');
+})
