de383f01590ad0dfbd2a5973dda78c0d0e71967d
alialaba <alialaba079@gmail.com>
2021-10-09 18:08:40 -0700

    skeletal html structure for section hero

--- js/index.js
+++ js/index.js
@@ -7,26 +7,3 @@ btnEl.addEventListener('click', () => {
 })
 
 // Sticky navigation
-// let sectionHeroEl = document.querySelector('.section-hero');
-// const obs = new IntersectionObserver(
-//     function (entries) {
-//         let ent = entries[0];
-//         console.log(ent);
-//         if (ent.isIntersecting === false) {
-//             document.body.classList.add('sticky');
-//         }
-
-//         if (ent.isIntersecting === true) {
-//             document.body.classList.remove('sticky')
-//         }
-
-//     },
-//     {
-//         // In the viewport
-//         root: null,
-//         threshold: 0,
-//         rootMargin: '-80px'
-
-//     }
-// );
-// obs.observe(sectionHeroEl);
