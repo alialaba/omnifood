83f6d3337fca9c1c9b69ffaebfa465292d2a47c2
alialaba <alialaba079@gmail.com>
2021-10-09 17:48:39 -0700

    html skeleton for step-text-1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,32 @@
+//navbar toggling
 let headerEl = document.querySelector('.header');
 let btnEl = document.querySelector('.btn-mobile-nav');
 
 btnEl.addEventListener('click', () => {
     headerEl.classList.toggle('nav-open');
 })
+
+// Sticky navigation
+// let sectionHeroEl = document.querySelector('.section-hero');
+// const obs = new IntersectionObserver(
+//     function (entries) {
+//         let ent = entries[0];
+//         console.log(ent);
+//         if (ent.isIntersecting === false) {
+//             document.body.classList.add('sticky');
+//         }
+
+//         if (ent.isIntersecting === true) {
+//             document.body.classList.remove('sticky')
+//         }
+
+//     },
+//     {
+//         // In the viewport
+//         root: null,
+//         threshold: 0,
+//         rootMargin: '-80px'
+
+//     }
+// );
+// obs.observe(sectionHeroEl);
