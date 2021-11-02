2923a2c0319ed2cccf053fa39cab25bb504a94d2
alialaba <alialaba079@gmail.com>
2021-11-01 17:50:15 -0700

    implemented a functionality for scrolling section

--- js/index.js
+++ js/index.js
@@ -2,9 +2,40 @@
 let headerEl = document.querySelector('.header');
 let btnEl = document.querySelector('.btn-mobile-nav');
 
+
 btnEl.addEventListener('click', () => {
     headerEl.classList.toggle('nav-open');
 })
+//select all a:link
+const allLinks = document.querySelectorAll("a:link")
+allLinks.forEach((link)=>{
+link.addEventListener('click',(e)=>{
+e.preventDefault();
+
+const href = link.getAttribute('href');
+
+// Scroll back to top
+if(href === "#"){
+    window.scrollTo({
+        top : 0,
+        behavior: 'smooth'
+    })
+}
+// Scroll to other links
+if(href !== '#' && href.startsWith('#')){
+    const sectionEl=document.querySelector(href)
+    sectionEl.scrollIntoView({behavior: 'smooth'});
+}
+
+
+// Close mobile naviagtion
+if(link.classList.contains('nav-list'))
+headerEl.classList.toggle("nav-open");
+
+})
+})
+
+
 
 // Sticky navigation
 let sectionHeroEl = document.querySelector('.section-hero');
